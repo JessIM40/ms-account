946ef827cd2792011b658334287b32bb76e2fd9b
Jess Cuellar Silva <jessinma40@gmail.com>
2024-11-14 10:47:34 -0500

    Added OpenAPI contract and server port in the Account microproject

---
+++ src/main/java/com/bootcamp/example/msaccountapi/MsAccountApiApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.example.msaccountapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAccountApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAccountApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/example/msaccountapi/MsAccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.example.msaccountapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAccountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
